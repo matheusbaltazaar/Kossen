c3fa15c3a9c9b428f3813039b40eac4f26ae9f82
matheusbaltazaar <matheusbaltazar@live.com>
2019-06-12 00:41:38 -0300

    Nova interface

--- app/src/main/java/com/baltazarstudio/kossen/database/Database.java
+++ app/src/main/java/com/baltazarstudio/kossen/database/Database.java
@@ -4,8 +4,6 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
-import com.baltazarstudio.kossen.ui.MainActivity;
-
 public class Database extends SQLiteOpenHelper {
 
 
--- app/src/main/java/com/baltazarstudio/kossen/ui/MainActivity.java
+++ app/src/main/java/com/baltazarstudio/kossen/ui/MainActivity.java
@@ -6,13 +6,13 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -30,27 +30,20 @@ public class MainActivity extends AppCompatActivity {
     private int totalSeconds = 0;
     private Runnable updateUIThread;
     private Runnable targetReachedThread;
-    private Button button;
-    private TextView lblChooseHour;
-    private TextView lblChooseMinute;
-    private TextView lblChooseSecond;
+    private Button buttonStartStop;
     private int totalGoalSeconds = 0;
     private boolean isGoalReached = false;
-    private MenuItem restartMenuItem;
-    private ImageButton buttonDecreaseHour;
-    private ImageButton buttonDecreaseMinute;
-    private ImageButton buttonDecreaseSecond;
-    private ImageButton buttonIncreaseHour;
-    private ImageButton buttonIncreaseMinute;
-    private ImageButton buttonIncreaseSecond;
+    private Button buttonReset;
+    private TextView tvGoalTime;
+    private Database database;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        button = findViewById(R.id.bt_start_stop_clock);
-        button.setOnClickListener(new View.OnClickListener() {
+        buttonStartStop = findViewById(R.id.bt_start_stop_time);
+        buttonStartStop.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 clockEnabled = !clockEnabled;
@@ -63,74 +56,76 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        tvTimer = findViewById(R.id.tv_clock_counter);
+        tvTimer = findViewById(R.id.tv_time_counter);
 
         targetReachedThread = initializeTargetReachedThread(this);
         updateUIThread = initializeUpdateUIThread();
 
+        tvGoalTime = findViewById(R.id.tv_goal_time);
 
-        buttonIncreaseHour = findViewById(R.id.button_increase_hour);
-        buttonIncreaseHour.setTag(R.id.button_increase_hour);
-        buttonIncreaseHour.setOnClickListener(new View.OnClickListener() {
+        TextView increaseOneHour = findViewById(R.id.button_increase_time_one_hour);
+        increaseOneHour.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                increase(buttonIncreaseHour);
+                increaseGoalTime(60);
+            }
+        });
+        TextView increaseThirtyMinutes = findViewById(R.id.button_increase_time_thirty_minutes);
+        increaseThirtyMinutes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                increaseGoalTime(30);
+            }
+        });
+        TextView increaseFifteenMinutes = findViewById(R.id.button_increase_time_fifteen_minutes);
+        increaseFifteenMinutes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                increaseGoalTime(15);
+            }
+        });
+        TextView increaseFiveMinutes = findViewById(R.id.button_increase_time_five_minutes);
+        increaseFiveMinutes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                increaseGoalTime(5);
+            }
+        });
+
+        buttonReset = findViewById(R.id.button_reset);
+        buttonReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (totalSeconds != 0) // Se o timer não foi iniciado
+                    createResetAlert();
+                else
+                    restoreClock();
             }
         });
 
 
-        buttonDecreaseHour = findViewById(R.id.button_decrease_hour);
-        buttonDecreaseHour.setTag(R.id.button_decrease_hour);
-        buttonDecreaseHour.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                decrease(buttonDecreaseHour);
-            }
-        });
+        database = new Database(this);
+    }
 
-        buttonIncreaseMinute = findViewById(R.id.button_increase_minute);
-        buttonIncreaseMinute.setTag(R.id.button_increase_minute);
-        buttonIncreaseMinute.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                increase(buttonIncreaseMinute);
-            }
-        });
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.options, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
 
-        buttonDecreaseMinute = findViewById(R.id.button_decrease_minute);
-        buttonDecreaseMinute.setTag(R.id.button_decrease_minute);
-        buttonDecreaseMinute.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                decrease(buttonDecreaseMinute);
-            }
-        });
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
 
-        buttonIncreaseSecond = findViewById(R.id.button_increase_second);
-        buttonIncreaseSecond.setTag(R.id.button_increase_second);
-        buttonIncreaseSecond.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                increase(buttonIncreaseSecond);
-            }
-        });
+        if (id == R.id.action_sound) {
+            startActivity(new Intent(this, SelectSoundActivity.class));
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
-        buttonDecreaseSecond = findViewById(R.id.button_decrease_second);
-        buttonDecreaseSecond.setTag(R.id.button_decrease_second);
-        buttonDecreaseSecond.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                decrease(buttonDecreaseSecond);
-            }
-        });
-
-        lblChooseHour = findViewById(R.id.lb_choose_hour);
-        lblChooseHour.setText(R.string.all_label_time_choose_placeholder);
-        lblChooseMinute = findViewById(R.id.lb_choose_minute);
-        lblChooseMinute.setText(R.string.all_label_time_choose_placeholder);
-        lblChooseSecond = findViewById(R.id.lb_choose_second);
-        lblChooseSecond.setText(R.string.all_label_time_choose_placeholder);
-
+    private void increaseGoalTime(int minutes) {
+        totalGoalSeconds += minutes * 60;
+        tvGoalTime.setText(formatLabelTimeString(totalGoalSeconds));
     }
 
     private Runnable initializeUpdateUIThread() {
@@ -138,11 +133,11 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void run() {
                 if (isGoalReached) {
-                    button.setText(R.string.all_button_resume);
+                    buttonStartStop.setText(R.string.all_button_resume);
                     toggleUIComponents(true);
                 } else {
-                    tvTimer.setText(formatLabelTimeString());
-                    restartMenuItem.setEnabled(false);
+                    tvTimer.setText(formatLabelTimeString(totalSeconds));
+                    toggleUIComponents(false);
                 }
             }
         };
@@ -158,107 +153,41 @@ public class MainActivity extends AppCompatActivity {
                 updateUI();
                 Toast.makeText(context, AppContext.getCurrentDateTime(), Toast.LENGTH_LONG).show();
 
-                showDialogGoalReached();
+                createDialogGoalReached();
             }
         };
     }
 
-    private void showDialogGoalReached() {
+    private void createDialogGoalReached() {
         final String tempo = AppContext.getCurrentDateTime();
 
         new AlertDialog.Builder(this)
+                .setCancelable(false)
                 .setTitle(R.string.all_label_goal_reached)
                 .setMessage(String.format("Tempo: %s", tempo))
                 .setNeutralButton("OK", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-                        new Database(MainActivity.this)
-                                .register(tempo);
+                        database.register(tempo);
+                        dialog.dismiss();
+                    }
+                })
+                .setOnDismissListener(new DialogInterface.OnDismissListener() {
+                    @Override
+                    public void onDismiss(DialogInterface dialog) {
+                        stopSound();
                     }
                 })
                 .create().show();
-    }
-
-
-    private void increase(ImageButton button) {
-        int id = (int) button.getTag();
-
-        if (id == R.id.button_increase_hour) {
-            int number = Integer.parseInt(lblChooseHour.getText().toString());
-
-            if (number == 23)
-                number = 0;
-            else
-                number++;
-
-            lblChooseHour.setText(number < 10 ? "0" + number : Integer.toString(number));
-
-        } else if (id == R.id.button_increase_minute) {
-            int number = Integer.parseInt(lblChooseMinute.getText().toString());
-
-            if (number == 59)
-                number = 0;
-            else
-                number++;
-
-            lblChooseMinute.setText(number < 10 ? "0" + number : Integer.toString(number));
-
-        } else if (id == R.id.button_increase_second) {
-            int number = Integer.parseInt(lblChooseSecond.getText().toString());
-
-            if (number == 59)
-                number = 0;
-            else
-                number++;
-
-            lblChooseSecond.setText(number < 10 ? "0" + number : Integer.toString(number));
-
-        }
-    }
-
-    private void decrease(ImageButton button) {
-        int id = (int) button.getTag();
-
-        if (id == R.id.button_decrease_hour) {
-            int number = Integer.parseInt(lblChooseHour.getText().toString());
-
-            if (number == 0)
-                number = 23;
-            else
-                number--;
-
-            lblChooseHour.setText(number < 10 ? "0" + number : Integer.toString(number));
-
-        } else if (id == R.id.button_decrease_minute) {
-            int number = Integer.parseInt(lblChooseMinute.getText().toString());
-
-            if (number == 0)
-                number = 59;
-            else
-                number--;
-
-            lblChooseMinute.setText(number < 10 ? "0" + number : Integer.toString(number));
-
-        } else if (id == R.id.button_decrease_second) {
-            int number = Integer.parseInt(lblChooseSecond.getText().toString());
-
-            if (number == 0)
-                number = 59;
-            else
-                number--;
-
-            lblChooseSecond.setText(number < 10 ? "0" + number : Integer.toString(number));
-
-        }
     }
 
     private void stopClock() {
         mainTimer.cancel();
 
         if (totalSeconds == 0 || isGoalReached)
-            button.setText(R.string.all_button_start);
+            buttonStartStop.setText(R.string.all_button_start);
         else
-            button.setText(R.string.all_button_resume);
+            buttonStartStop.setText(R.string.all_button_resume);
 
         toggleUIComponents(true);
     }
@@ -266,15 +195,12 @@ public class MainActivity extends AppCompatActivity {
     private void resumeClock() {
         mainTimer = new Timer();
         mainTimer.scheduleAtFixedRate(initializeTimer(), 0, 1000);
-        button.setText(R.string.all_button_stop);
+        buttonStartStop.setText(R.string.all_button_stop);
     }
 
     private void restoreClock() {
         stopClock();
         tvTimer.setText(R.string.tv_clock_label_placeholder);
-        lblChooseHour.setText(R.string.all_label_time_choose_placeholder);
-        lblChooseMinute.setText(R.string.all_label_time_choose_placeholder);
-        lblChooseSecond.setText(R.string.all_label_time_choose_placeholder);
         totalSeconds = 0;
         clockEnabled = false;
         isGoalReached = false;
@@ -282,17 +208,22 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void toggleUIComponents(boolean enabled) {
-        restartMenuItem.setEnabled(enabled);
+//        restartMenuItem.setEnabled(enabled);
+        buttonReset.setEnabled(enabled);
+
+        if (enabled) {
+            buttonReset.setBackgroundResource(R.drawable.all_rounded_border_button_green);
+            buttonReset.setTextColor(ContextCompat.getColor(this, R.color.green));
+        } else {
+            buttonReset.setBackgroundResource(R.drawable.all_rounded_border_button_gray);
+            buttonReset.setTextColor(ContextCompat.getColor(this, android.R.color.darker_gray));
+        }
     }
 
     private TimerTask initializeTimer() {
         return new TimerTask() {
             @Override
             public void run() {
-                if (totalSeconds == 0) {
-                    defineTimeTarget();
-                }
-
                 updateUI();
                 incrementTotalSeconds();
             }
@@ -307,7 +238,7 @@ public class MainActivity extends AppCompatActivity {
         this.runOnUiThread(targetReachedThread);
     }
 
-    private String formatLabelTimeString() {
+    private String formatLabelTimeString(int totalSeconds) {
         String clock;
         if (totalSeconds < 60) {
             clock = "00:00:"
@@ -334,17 +265,6 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return clock;
-    }
-
-    private void defineTimeTarget() {
-        int seconds = Integer.parseInt(lblChooseSecond.getText().toString());
-        int minutes = Integer.parseInt(lblChooseMinute.getText().toString());
-        int hours = Integer.parseInt(lblChooseHour.getText().toString());
-
-        totalGoalSeconds = seconds
-                + minutes * 60
-                + hours * 3600;
-
     }
 
     private void incrementTotalSeconds() {
@@ -378,34 +298,22 @@ public class MainActivity extends AppCompatActivity {
         mediaPlayer.start();
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.options, menu);
-        restartMenuItem = menu.findItem(R.id.action_restart);
-        return super.onCreateOptionsMenu(menu);
+    private void stopSound() {
+        // TODO
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-
-        if (id == R.id.action_restart) {
-            new AlertDialog.Builder(this)
-                    .setTitle("Confirmação")
-                    .setMessage("Quer mesmo reiniciar a contagem ?")
-                    .setPositiveButton("Sim", new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
-                            restoreClock();
-                        }
-                    })
-                    .setNegativeButton("Não", null)
-                    .create().show();
-        } else if (id == R.id.action_sound) {
-            startActivity(new Intent(this, SelectSoundActivity.class));
-        }
-
-        return super.onOptionsItemSelected(item);
+    private void createResetAlert() {
+        new AlertDialog.Builder(this)
+                .setTitle("Confirmação")
+                .setMessage("Quer mesmo reiniciar a contagem ?")
+                .setPositiveButton("Sim", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        restoreClock();
+                    }
+                })
+                .setNegativeButton("Não", null)
+                .create().show();
     }
 
 }
